4541e64a8344a5ee272f1b8f967d80711546851c
Kattenelvis <officialkattenelvis@gmail.com>
2022-08-16 17:35:47 +0200

    w

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from json.encoder import INFINITY
 from distance_matricies import calculate_distance_matricies, calculate_shortest_distance_matrix, calculate_distance_matrix
 from visualizations import display_distance_matrix
 
@@ -8,10 +9,10 @@ number_of_ones = 4
 positions = [(x, y) for x in range(x_size) for y in range(y_size)]
 
 distance_matricies = []
-distance_matricies_average_distance = []
-distance_matricies_average_distance_smallest = []
+distance_matricies_average = []
+distance_matricies_average_largest = [(-INFINITY,0) for i in range(number_of_ones)]
 
-for position in positions:
+for k, position in enumerate(positions):
     distance_matrix = calculate_distance_matrix(position, x_size, y_size)
     # distance_matricies.append(distance_matrix)
     average_distance = 0
@@ -20,9 +21,29 @@ for position in positions:
             average_distance += distance_matrix[x][y]
 
     average_distance /= x_size * y_size - 1
-    distance_matricies_average_distance.append(average_distance)
+    distance_matricies_average.append(average_distance)
+    
+    # All DMADS arrays have larger numbers first [5,4,3,2]
+    # If a new distance shows up like 3.5 then it bubbles up to it's position
+    #[4,3.5,3,2]
+    if (average_distance > distance_matricies_average_largest[0][0]):
+        distance_matricies_average_largest[0] = (average_distance, k)
+        for i in range(1, number_of_ones):
+            if average_distance > distance_matricies_average_largest[i][0]:
+                distance_matricies_average_largest[i-1] = distance_matricies_average_largest[i]
+                distance_matricies_average_largest[i] = (average_distance, k)
+    
 
-print(min(distance_matricies_average_distance))
+    print(distance_matricies_average_largest)
+
+print(distance_matricies)
+
+matricies_with_largest_average_distance = []
+for distance_matrix in distance_matricies_average_largest:
+    print(positions[distance_matrix[1]])
+    matricies_with_largest_average_distance.append(positions[distance_matrix[1]])
+
+calculate_shortest_distance_matrix(matricies_with_largest_average_distance)
 
 
 ones = [(1,1)]
